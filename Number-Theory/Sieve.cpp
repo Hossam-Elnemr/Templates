270662f09f424c5728d1dd9b75518deed6ad9ac5
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-03-07 20:35:04 +0200

    Update Sieve.cpp

--- Number-Theory/Sieve.cpp
+++ Number-Theory/Sieve.cpp
@@ -1,11 +1,11 @@
 const int N = 1e5;
 bitset<N> notPrime;
 void sieve() {
-    notPrime[0] = notPrime[1] = 1;
+    notPrime[0] = notPrime[1] = true;
     for(long long i = 2; i*i<N; ++i) {
         if(notPrime[i]) continue;
 
         for(long long j = i*i; j<N; j+=i)
-            notPrime[j] = 1;
+            notPrime[j] = true;
     }
 }
