8714600fec434e482bfa2c253445a580d6ed4a5a
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-14 23:24:42 +0200

    Update Combinatorics.cpp

--- Number-Theory/Combinatorics.cpp
+++ Number-Theory/Combinatorics.cpp
@@ -1,4 +1,6 @@
-long long fact[200005];
+int MOD = 1e9+7;
+const int N = 5005;
+long long fact[N], factinv[N];
 long long subt(long long a, long long b) { return ((a % MOD) - (b % MOD) + MOD) % MOD; }
 long long add(long long a, long long b) { return ((a % MOD) + (b % MOD)) % MOD; }
 long long multi(long long a, long long b) { return ((a % MOD) * (b % MOD)) % MOD; }
@@ -14,17 +16,20 @@ long long fp(long long b, long long exp) {
     return res;
 }
 long long INV(long long a) { return fp(a, MOD - 2); }
+long long divi(long long a, long long b) { return multi(a, INV(b)); }
 void calcFactorial() {
     fact[0] = 1;
-    for (long long i = 1; i<200005; ++i)
+    for (long long i = 1; i<N; ++i)
         fact[i] = multi(fact[i-1], i);
+    factinv[N-1] = INV(fact[N-1]);
+    for (int i = N-1; i>0; --i)
+        factinv[i-1] = multi(factinv[i], i);
 }
-long long divi(long long a, long long b) { return multi(a, INV(b)); }
 long long nCr(long long n, long long r) {
     if (r>n || r<0 || n<0)
         return 0;
     if (r == n || r == 0)
         return 1;
-    return divi(fact[n], multi(fact[r], fact[n-r]));
+    return multi(fact[n], multi(factinv[r], factinv[n-r]));
 }
 long long starsNBars(long long n, long long k) { return nCr(n+k-1, n); }
