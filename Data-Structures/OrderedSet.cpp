d29c0d58d7f1c21e7d8fda03221127f8c6c7d40a
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-06-28 22:33:42 +0300

    Update OrderedSet.cpp

--- Data-Structures/OrderedSet.cpp
+++ Data-Structures/OrderedSet.cpp
@@ -1,4 +1,5 @@
-# include <ext/pb_ds/assoc_container.hpp>
-# include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-# define ordered_multiset tree<int, null_type, less_equal /*(equal) only for set not multiset*/<int>, rb_tree_tag, tree_order_statistics_node_update>
+template<class T> using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, std::less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
