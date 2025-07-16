5ac72ff7d6e1902d793df7bb35ad4c83fcec2a1e
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-07-16 20:18:02 +0300

    Update Floyd.cpp

--- Data-Structures/Floyd.cpp
+++ Data-Structures/Floyd.cpp
@@ -1,18 +1,17 @@
 const int N = 504;
-int dp[N][N], adj[N][N];
+vector<vector<int>> dist(N, vector<int>(N, 1e14));
 void floyd(int n) {
 	for(int i = 1; i<=n; ++i)
-		for(int j = 1; j<=n; ++j)
-			adj[i][j] = (i == j? 0 : 1e13);
+		dist[i][i] = 0;
 
 	// take input (adjacency matrix)
 	// while(m--) {
 	// 	int u, v, c; cin >> u >> v >> c;
-	// 	adj[u][v] = min(adj[u][v], c);
-	// 	adj[v][u] = min(adj[v][u],c);
+	// 	dist[u][v] = min(dist[u][v], c);
+	// 	dist[v][u] = min(dist[v][u],c);
 	// }
 	for(int k = 1; k<=n; ++k)
 		for(int i = 1; i<=n; ++i)
 			for(int j = 1; j<=n; ++j)
-				dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 }
