fa8a700b2fd883135d1a2648ca7921bb2ec4b891
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2026-02-13 21:54:16 +0200

    Refactor multiplication and division functions

--- Math/Number-Theory/Combinatorics.cpp
+++ Math/Number-Theory/Combinatorics.cpp
@@ -1,9 +1,9 @@
 int MOD = 1e9+7;
-const int N = 5005;
+const int N = 1e5 + 5;
 long long fact[N], factinv[N];
 long long subt(long long a, long long b) { return ((a % MOD) - (b % MOD) + MOD) % MOD; }
 long long add(long long a, long long b) { return ((a % MOD) + (b % MOD)) % MOD; }
-long long multi(long long a, long long b) { return ((a % MOD) * (b % MOD)) % MOD; }
+long long mul(long long a, long long b) { return ((a % MOD) * (b % MOD)) % MOD; }
 long long fp(long long b, long long exp) {
     if(exp == 0)
         return 1;
@@ -14,20 +14,20 @@ long long fp(long long b, long long exp) {
     return res;
 }
 long long INV(long long a) { return fp(a, MOD - 2); }
-long long divi(long long a, long long b) { return multi(a, INV(b)); }
+long long divi(long long a, long long b) { return mul(a, INV(b)); }
 void calcFactorial() {
     fact[0] = 1;
     for (long long i = 1; i<N; ++i)
-        fact[i] = multi(fact[i-1], i);
+        fact[i] = mul(fact[i-1], i);
     factinv[N-1] = INV(fact[N-1]);
     for (int i = N-1; i>0; --i)
-        factinv[i-1] = multi(factinv[i], i);
+        factinv[i-1] = mul(factinv[i], i);
 }
 long long nCr(long long n, long long r) {
     if (r>n || r<0 || n<0)
         return 0;
     if (r == n || r == 0)
         return 1;
-    return multi(fact[n], multi(factinv[r], factinv[n-r]));
+    return mul(fact[n], mul(factinv[r], factinv[n-r]));
 }
 long long starsNBars(long long n, long long k) { return nCr(n+k-1, n); }
