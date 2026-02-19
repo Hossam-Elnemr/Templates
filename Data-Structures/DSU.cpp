9c8b93dd3e2c5bd0bc6ba2abcc09ccae5f9b903c
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2026-02-19 08:50:56 +0200

    Enhance DSU with rollback support and comments
    Added rollback functionality and path compression note.

--- Data-Structures/DSU.cpp
+++ Data-Structures/DSU.cpp
@@ -1,5 +1,6 @@
 struct DSU {
     vector<int> sz, par;
+    stack<array<int, 4>> st;
     DSU(int n) {
         sz.resize(n + 1, 1);
 
@@ -8,15 +9,29 @@ struct DSU {
     }
 
     int find(int u) { // ~O(1)
-        return par[u] = u == par[u]? u : find(par[u]);
+        return par[u] = u == par[u]? u : find(par[u]); // par[u] = ..., this is called path compression
     }
     void unite(int u, int v) {
         u = find(u), v = find(v);
         if (u == v)
-            return;
+            return st.push({-1, -1, -1, -1});
+        
         if (sz[u] > sz[v]) // always merge u into v (small to large)
             swap(u, v);
+        st.push({u, par[u], v, sz[v]});
+
         sz[v] += sz[u];
         par[u] = v;
     }
+
+    //         *** NOTE ***
+    //   If you you want to use rollback, remove path compression first
+    // void undo() {
+    //     array<int, 4> cur = st.top();
+    //     st.pop();
+    //     if (cur[0] == -1)
+    //         return;
+    //     par[cur[0]] = cur[1];
+    //     sz[cur[2]] = cur[3];
+    // }
 };
