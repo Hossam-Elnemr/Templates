1a14040d22247029ce4eadb5da6022d665c01204
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2026-02-27 21:53:19 +0200

    Refactor Sqrt Decomposition implementation

--- Data-Structures/Sqrt Decomposition.cpp
+++ Data-Structures/Sqrt Decomposition.cpp
@@ -1,25 +1,46 @@
-class Sqrt {
-public:
-    vector<int> a, blocks;
-    int SQ;
-    Sqrt(int n) {
-        SQ = sqrt(n) + 1;
-        a.resize(n, 0);
-        blocks.resize(SQ, 0);
+const int N = 2e5 + 5, SQ = 325;
+int a[N], n;
+long long buckets[N / SQ];
+
+void build(int x) { // builds a single block
+    buckets[x] = 1e11;
+    int start = x * SQ, end = min((x + 1) * SQ, n);
+    for (int i = start; i<end; ++i)
+        buckets[x] = min(buckets[x], a[i] * 1ll);
+}
+
+void update(int i, int val) { // a[i] = val
+    a[i] = val;
+    build(i / SQ); // build my block (block contains i)
+}
+
+int get(int l, int r) { // min(a[l...r])
+    long long ans = 1e11;
+    for (int i = l; i<=r; ) {
+        if (i%SQ == 0 && i + SQ - 1 <= r) {
+            ans = min(ans, buckets[i / SQ]);
+            i += SQ;
+        }
+        else {
+            ans = min(ans, a[i] * 1ll);
+            ++i;
+        }
     }
-    void set(int i, int val) {
-        blocks[i/SQ] += (val - a[i]);
-        a[i] = val;
+    return ans;
+}
+
+void solve() {
+    cin >> n;
+    for (int i = 0; i<n; ++i)
+        cin >> a[i];
+
+    // Builds all the blocks
+    for (int i = 0; i<n; i += SQ) 
+        build(i / SQ);
+
+    int q; cin >> q;
+    while (q--) {
+        int l, r; cin >> l >> r;
+        cout << get(l, r) << '\n';
     }
-    int get(int l, int r) {
-        int ans = 0;
-        int begin = l/SQ, end = r/SQ;
-        for (int i = begin; i<end; ++i)
-            ans += blocks[i];
-        for (int i = begin*SQ; i<l; ++i)
-            ans -= a[i];
-        for (int i = r; i>=end*SQ; --i)
-            ans += a[i];
-        return ans;
-    }
-};
+}
