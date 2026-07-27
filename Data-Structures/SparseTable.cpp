08c29805d468a2ded3b931fd7b5e3f98a14a55c2
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2026-07-27 07:21:05 +0300

    Refactor SparseTable to use pairs for storage

--- Data-Structures/SparseTable.cpp
+++ Data-Structures/SparseTable.cpp
@@ -1,34 +1,37 @@
-template<typename T, class CMP = function<T(const T &, const T &)>>
 class SparseTable {
 public:
     int n{};
     vector<int> lg;              // precomputed logs
-    vector<vector<T>> sp;
-    CMP func;
-
-    void build(const vector<T> &a, const CMP &f) {
-        func = f;
+    vector<vector<pair<long long, int>>> sp;
+ 
+    pair<long long, int> merge(pair<long long, int> a, pair<long long, int> b) {
+        if (a.first > b.first)
+            return a;
+        if (b.first > a.first)
+            return b;
+        return {a.first, min(a.second, b.second)};
+    }
+    void build(const vector<pair<long long, int>> &a) {
         n = (int)a.size();
-
-        // precompute logs: lg[i] = floor(log2(i))
+ 
         lg.assign(n + 1, 0);
         for (int i = 2; i <= n; ++i) lg[i] = lg[i / 2] + 1;
-
+ 
         int max_log = lg[n] + 1;
         sp.assign(max_log, {});
         sp[0] = a;
-
+ 
         for (int j = 1; j < max_log; ++j) {
             int len = 1 << j;
             sp[j].resize(n - len + 1);
             for (int i = 0; i + len <= n; ++i) {
-                sp[j][i] = func(sp[j - 1][i], sp[j - 1][i + (len >> 1)]);
+                sp[j][i] = merge(sp[j - 1][i], sp[j - 1][i + (len >> 1)]);
             }
         }
     }
-
-    T get(int l, int r) const {
+ 
+    pair<long long, int> get(int l, int r) {
         int k = lg[r - l + 1];
-        return func(sp[k][l], sp[k][r - (1 << k) + 1]);
+        return merge(sp[k][l], sp[k][r - (1 << k) + 1]);
     }
 };
