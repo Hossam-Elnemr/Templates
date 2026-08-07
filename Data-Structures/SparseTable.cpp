3392dafe45888ad1e485ab7360b29de4da403134
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2026-08-07 23:39:35 +0300

    Refactor SparseTable to use long long and max function

--- Data-Structures/SparseTable.cpp
+++ Data-Structures/SparseTable.cpp
@@ -2,16 +2,9 @@ class SparseTable {
 public:
     int n{};
     vector<int> lg;              // precomputed logs
-    vector<vector<pair<long long, int>>> sp;
+    vector<vector<long long>> sp;
  
-    pair<long long, int> merge(pair<long long, int> a, pair<long long, int> b) {
-        if (a.first > b.first)
-            return a;
-        if (b.first > a.first)
-            return b;
-        return {a.first, min(a.second, b.second)};
-    }
-    void build(const vector<pair<long long, int>> &a) {
+    void build(const vector<long long> &a) {
         n = (int)a.size();
  
         lg.assign(n + 1, 0);
@@ -25,13 +18,13 @@ public:
             int len = 1 << j;
             sp[j].resize(n - len + 1);
             for (int i = 0; i + len <= n; ++i) {
-                sp[j][i] = merge(sp[j - 1][i], sp[j - 1][i + (len >> 1)]);
+                sp[j][i] = max(sp[j - 1][i], sp[j - 1][i + (len >> 1)]);
             }
         }
     }
  
-    pair<long long, int> get(int l, int r) {
+    long long get(int l, int r) {
         int k = lg[r - l + 1];
-        return merge(sp[k][l], sp[k][r - (1 << k) + 1]);
+        return max(sp[k][l], sp[k][r - (1 << k) + 1]);
     }
 };
